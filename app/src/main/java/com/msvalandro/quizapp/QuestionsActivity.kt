fb331854afc7b71a8a16fec0ae949def96a108fe
Matheus Valandro <msvalandro@proton.me>
2024-10-18 19:00:47 +0100

    feat: connect question UI to model

--- app/src/main/java/com/msvalandro/quizapp/QuestionsActivity.kt
+++ app/src/main/java/com/msvalandro/quizapp/QuestionsActivity.kt
@@ -2,12 +2,25 @@ package com.msvalandro.quizapp
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class QuestionsActivity : AppCompatActivity() {
+    private var progressBar: ProgressBar? = null
+    private var textViewProgress: TextView? = null
+    private var textViewQuestion: TextView? = null
+    private var imageViewFlag: ImageView? = null
+
+    private var textViewOptionOne: TextView? = null
+    private var textViewOptionTwo: TextView? = null
+    private var textViewOptionThree: TextView? = null
+    private var textViewOptionFour: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -19,7 +32,30 @@ class QuestionsActivity : AppCompatActivity() {
             insets
         }
 
+        progressBar = findViewById(R.id.progress_bar)
+        textViewProgress = findViewById(R.id.text_view_progress)
+        textViewQuestion = findViewById(R.id.text_view_question)
+        imageViewFlag = findViewById(R.id.image_view_flag)
+
+        textViewOptionOne = findViewById(R.id.text_view_option_one)
+        textViewOptionTwo = findViewById(R.id.text_view_option_two)
+        textViewOptionThree = findViewById(R.id.text_view_option_three)
+        textViewOptionFour = findViewById(R.id.text_view_option_four)
+
+
         val questionsList = Constants.getQuestions()
-        Log.i("Questions list size is", "${questionsList.size}")
+
+        var currentPosition = 1
+        val question = questionsList[currentPosition - 1]
+
+        progressBar?.progress = currentPosition
+        textViewProgress?.text = "$currentPosition / ${progressBar?.max}"
+        textViewQuestion?.text = question.question
+        imageViewFlag?.setImageResource(question.image)
+
+        textViewOptionOne?.text = question.optionOne
+        textViewOptionTwo?.text = question.optionTwo
+        textViewOptionThree?.text = question.optionThree
+        textViewOptionFour?.text = question.optionFour
     }
 }
