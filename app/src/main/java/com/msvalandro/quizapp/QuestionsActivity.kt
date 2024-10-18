29c733410e6b36c27f8cfaa11296aaa7f9f2f8b0
Matheus Valandro <msvalandro@proton.me>
2024-10-18 19:24:51 +0100

    feat: add question options logic

--- app/src/main/java/com/msvalandro/quizapp/QuestionsActivity.kt
+++ app/src/main/java/com/msvalandro/quizapp/QuestionsActivity.kt
@@ -1,16 +1,26 @@
 package com.msvalandro.quizapp
 
+import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class QuestionsActivity : AppCompatActivity() {
+class QuestionsActivity : AppCompatActivity(), OnClickListener {
+    private var stateCurrentPosition: Int = 1
+    private var stateQuestionsList: ArrayList<Question>? = null
+    private var stateSelectedOptionPosition: Int = 0
+
     private var progressBar: ProgressBar? = null
     private var textViewProgress: TextView? = null
     private var textViewQuestion: TextView? = null
@@ -20,6 +30,8 @@ class QuestionsActivity : AppCompatActivity() {
     private var textViewOptionTwo: TextView? = null
     private var textViewOptionThree: TextView? = null
     private var textViewOptionFour: TextView? = null
+
+    private var buttonSubmit: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,14 +54,28 @@ class QuestionsActivity : AppCompatActivity() {
         textViewOptionThree = findViewById(R.id.text_view_option_three)
         textViewOptionFour = findViewById(R.id.text_view_option_four)
 
+        buttonSubmit = findViewById(R.id.button_submit)
 
-        val questionsList = Constants.getQuestions()
+        textViewOptionOne?.setOnClickListener(this)
+        textViewOptionTwo?.setOnClickListener(this)
+        textViewOptionThree?.setOnClickListener(this)
+        textViewOptionFour?.setOnClickListener(this)
+        buttonSubmit?.setOnClickListener(this)
 
-        var currentPosition = 1
-        val question = questionsList[currentPosition - 1]
+        stateQuestionsList = Constants.getQuestions()
 
-        progressBar?.progress = currentPosition
-        textViewProgress?.text = "$currentPosition / ${progressBar?.max}"
+        setQuestion()
+    }
+
+    private fun setQuestion() {
+        if (stateQuestionsList.isNullOrEmpty()) {
+            return
+        }
+
+        val question = stateQuestionsList!![stateCurrentPosition - 1]
+
+        progressBar?.progress = stateCurrentPosition
+        textViewProgress?.text = "$stateCurrentPosition / ${progressBar?.max}"
         textViewQuestion?.text = question.question
         imageViewFlag?.setImageResource(question.image)
 
@@ -57,5 +83,78 @@ class QuestionsActivity : AppCompatActivity() {
         textViewOptionTwo?.text = question.optionTwo
         textViewOptionThree?.text = question.optionThree
         textViewOptionFour?.text = question.optionFour
+
+        if (stateCurrentPosition == stateQuestionsList!!.size) {
+            buttonSubmit?.text = "FINISH"
+        } else {
+            buttonSubmit?.text = "SUBMIT"
+        }
+    }
+
+    private fun defaultOptionsView() {
+        val options = ArrayList<TextView>()
+
+        textViewOptionOne?.let {
+            options.add(0, it)
+        }
+        textViewOptionTwo?.let {
+            options.add(1, it)
+        }
+        textViewOptionThree?.let {
+            options.add(2, it)
+        }
+        textViewOptionFour?.let {
+            options.add(3, it)
+        }
+
+        options.forEach {
+            it.setTextColor(Color.parseColor("#7A8089"))
+            it.typeface = Typeface.DEFAULT
+            it.background = ContextCompat.getDrawable(
+                this,
+                R.drawable.default_option_border_bg
+            )
+        }
+    }
+
+    private fun selectedOptionView(textView: TextView, selectedOptionNumber: Int) {
+        defaultOptionsView()
+
+        stateSelectedOptionPosition = selectedOptionNumber
+
+        textView.setTextColor(Color.parseColor("#363A43"))
+        textView.setTypeface(textView.typeface, Typeface.BOLD)
+        textView.background = ContextCompat.getDrawable(
+            this,
+            R.drawable.selected_option_border_bg
+        )
+    }
+
+    override fun onClick(view: View?) {
+        when (view?.id) {
+            R.id.text_view_option_one -> {
+                textViewOptionOne?.let {
+                    selectedOptionView(it, 1)
+                }
+            }
+            R.id.text_view_option_two -> {
+                textViewOptionTwo?.let {
+                    selectedOptionView(it, 2)
+                }
+            }
+            R.id.text_view_option_three -> {
+                textViewOptionThree?.let {
+                    selectedOptionView(it, 3)
+                }
+            }
+            R.id.text_view_option_four -> {
+                textViewOptionFour?.let {
+                    selectedOptionView(it, 4)
+                }
+            }
+            R.id.button_submit -> {
+//                TODO: implement button submit
+            }
+        }
     }
 }
